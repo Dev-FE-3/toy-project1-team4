ba762437af64c1bb780eccb690289aa8b40a5bb4
김민성 <minsung@gimminseong-ui-noteubug.local>
2025-01-15 19:45:42 +0900

    feat: 직원 리스트 페이지 퍼블리싱 작업

--- src/pages/admin/employee/list/employee-list.js
+++ src/pages/admin/employee/list/employee-list.js
@@ -1,10 +1,101 @@
 import './employee-list.css';
 
-export const employeeList = function (content) {
+// const renderList = function (list) {}
 
+export const employeeList = function (content) {
   content.innerHTML = `
     <div id="employee-list">
-      employee-list
+      <section id="employee-tile">
+        <div id="employee-tile__action-button">
+          <div class="button-container">
+            <button type="button" class="btn btn--secondary">csv 저장</button>
+            <button type="button" class="btn btn--secondary">excel 저장</button>
+          </div>
+          <div class="button-container">
+            <button type="button" class="btn btn--primary">직원 목록 편집</button>
+          </div>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col" class="employee-table__name">
+                이름
+              </th>
+              <th scope="col" class="employee-table__id">
+                사원번호
+              </th>
+              <th scope="col" class="employee-table__department">
+                소속
+              </th>
+              <th scope="col" class="employee-table__position">
+                직급
+              </th>
+              <th scope="col" class="employee-table__status">
+                근무 상태
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="employee-table__name">송하영</td>
+              <td class="employee-table__id">12345678</td>
+              <td class="employee-table__department">인사팀</td>
+              <td class="employee-table__position">대리</td>
+              <td>
+                <span class="label label--red employee-table__status">
+                  결근
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>내용1</td>
+              <td>내용2</td>
+              <td>내용3</td>
+              <td>내용3</td>
+              <td>
+                <span class="label label--orange">휴가</span>
+              </td>
+            </tr>
+            <tr>
+              <td>내용1</td>
+              <td>내용2</td>
+              <td>내용3</td>
+              <td>내용3</td>
+              <td>
+                <span class="label label--green">근무중</span>
+              </td>
+            </tr>
+            <tr>
+              <td>내용1</td>
+              <td>내용2</td>
+              <td>내용3</td>
+              <td>내용3</td>
+              <td>
+                <span class="label label--purple">자리비움</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section class="pagination">
+        <a href="#" class="pagination--prev">
+          prev
+        </a>
+        <a href="#" class="active">
+          1
+        </a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">7</a>
+        <a href="#">8</a>
+        <a href="#" class="pagination--next">
+          next
+        </a>
+      </section>
     </div>
-  `;
-}
+    `;
+};
