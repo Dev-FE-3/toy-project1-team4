09d224caec180bbfd14c2b53e51565bd30024503
minsung <mingsung521@gmail.com>
2025-01-16 18:44:40 +0900

    fix: 공통컴포넌트 변경점 -  box class 적용

--- src/pages/admin/employee/info/employee-info.js
+++ src/pages/admin/employee/info/employee-info.js
@@ -9,60 +9,67 @@ export const employeeInfo = function (content) {
 
   content.innerHTML = `
 <div id="employee-info">
-  <section class="tile-container col-7">
-    <div class="section tile__name-photo">
-      <div class="tile__name-photo-contents">
+  <section class="col-7">
+    <div class="box name-photo">
+      <div class="name-photo__contents">
         <div class="contents--background-img">
-        <img 
-        src="../../../../../public/images/profile_face.png" alt="profile image" class="contents--profile-img"
-        />
+          <img
+            src="../../../../../public/images/profile_face.png"
+            alt="profile image"
+            class="contents--profile-img"
+          />
         </div>
         <h5 class="contents--name">Jeffrey Bezos</h5>
         <p class="contents--position">CS 팀 ∙ 고객 성공 매니저</p>
       </div>
-      <div class="tile__edit-details">
+      <div class="name-photo__edit-details">
         <span class="edit-details__date">2024.12.03 수정됨</span>
-        <button type="button" class="btn btn--primary edit-details__btn">프로필 사진 수정하기</button>
+        <button type="button" class="btn btn--primary">
+          프로필 사진 수정하기
+        </button>
       </div>
     </div>
   </section>
-  <section class="tile-container col-5">
-    <div class="section tile__work-period">
-      <div class="section__top">
-        <h5 class="section__title">근무 정보</h5>
-        <a href="#" class="section__more">
+  <section class="col-5">
+    <div class="box work-period">
+      <div class="box__top">
+        <h5 class="box__title">근무 정보</h5>
+        <a href="#" class="box__more">
           더보기
         </a>
       </div>
-      <div class="section__bottom tile__contents">
-      <p class="work-period__text">${serviceYears}년 ${serviceMonths}개월</p>
+      <div class="box__bottom box__contents">
+        <p class="work-period__text">
+          ${serviceYears}년 ${serviceMonths}개월
+        </p>
       </div>
     </div>
-    <div class="section tile__contact">
-      <div class="section__top">
-        <h5 class="section__title">연락처</h5>
-        <a href="#" class="section__more">
+    <div class="box contact">
+      <div class="box__top">
+        <h5 class="box__title">연락처</h5>
+        <a href="#" class="box__more">
           더보기
         </a>
       </div>
-      <div class="section__bottom tile__contents">
-      <p class="contact__text">${conatctCall}</p>
-      <p class="contact__text">${conatctEmail}</p>
+      <div class="box__bottom box__contents">
+        <p class="contact__text">${conatctCall}</p>
+        <p class="contact__text">${conatctEmail}</p>
       </div>
     </div>
-    <div class="section tile__performance">
-      <div class="section__top">
-        <h5 class="section__title">직원평가</h5>
-        <a href="#" class="section__more">
+    <div class="box performance">
+      <div class="box__top">
+        <h5 class="box__title">직원평가</h5>
+        <a href="#" class="box__more">
           더보기
         </a>
       </div>
-      <div class="section__bottom tile__contents">
-      <p class="performance__text"><span class="performance__text--my-rate">${performanceRate}</span>/5</p>
+      <div class="box__bottom box__contents">
+        <p class="performance__text">
+          <span class="performance__text--my-rate">${performanceRate}</span>/5
+        </p>
       </div>
     </div>
   </section>
 </div>
-
   `;
 };
