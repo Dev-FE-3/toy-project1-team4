79fe523a1f5116458419095441f9342190861166
hgTime <hgtime@gmail.com>
2025-01-17 18:33:02 +0900

    feat: 공지사항 마크업

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -4,40 +4,31 @@ export const notice = function (content) {
 
   content.innerHTML = `
     <ul id="notice" class="row">
-      <li class = "card col-4">
-      <div class="image-container">
-        <img src="./../public/images/img_IT.png" alt="게시글 이미지" class="card__img" />
-      </div> 
-      <div>
-        <section class="card__title">
-          <p>[신규 프로젝트 런칭 안내: Project Phoenix]</p>
-        </section>
-
-        <section class="card__content">
-          <p>
-            본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
-          </p>
-        </section>
-      </div>
-      
-      <section class="card__author">
-        <div class="author-content">
-          <img src="./../public/images/img_men.png" />
-          <div>
-            <div class="author-info">
-              <span class="author__position">직책</span>
-              <span class="author__name">이름</span>
-            </div>
-
-            <time>
-              2025-01-14
-            </time>
+      <li class= "card col-4 ">
+        <a href="#">
+          <div class="image-container">
+            <div src="./../public/images/img_IT.png" alt="게시글 이미지" class="card__img" /> 
+          </div> 
+          <div class="">
+            <h4 class="">[신규 프로젝트 런칭 안내: Project Phoenix]</h4>
+            <p class="card__content">
+              본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
+            </p>
           </div>
-        </div>
-      </section>
+          <div class="card__author">
+            <img src="./../public/images/img_men.png" class="author-img" />
+            <div class="">
+              <div class="author-content">
+                <span class="author__position">백그라운드개발드림팀</span>
+                <span class="author__name">김수완</span>
+              </div>
+              <time>
+                2025-01-14
+              </time>
+            </div>
+          </div>
+        </a>
       </li>
-      
     </ul>
-    
   `;
 }
