1b0e1c678710a0560035e6e23dce6e4f90afec66
jungu0416 <jungu0416@naver.com>
2025-01-17 12:35:46 +0900

    feat : 로그인 PR 리뷰 수정.ver1

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,19 +5,23 @@ export const login = function (content) {
   content.innerHTML = `
     <section id="login">
       <div class="logo">
-        <img src="./../public/images/img_logo.png" class="logo__img" alt="PPangGeut">
+        <a href="/" class="logo__link">
+          <img src="./../public/images/img_logo.png" class="logo__img" alt="PPangGeut">
+        </a>
       </div>
       <div class="login--form">
         <form action="javascipt:void(0)" method="post">
             <p class="login--form__title">로그인</p>
-            <div class="input-wrap">
-                <label for="input-01" class="input-label">아이디</label>
-                <input type="text" class="input-text" id="input-01" placeholder="사원번호 또는 이메일 주소를 입력해주세요">
+            <div class="input-wrap form__id">
+                <label for="login__id" class="input-label">아이디</label>
+                <input type="text" class="input-text" id="login__id" placeholder="사원번호 또는 이메일 주소를 입력해주세요">
             </div>    
-            <div class="input-wrap">
-              <label for="input-01" class="input-label">패스워드</label>
-              <input type="password" class="input-text" id="input-01" placeholder="">
-              <a href="javascript:void(0)">아이디/패스워드 찾기</a>
+            <div class="input-wrap form__pw">
+              <label for="login__pw" class="input-label">패스워드</label>
+              <input type="password" class="input-text" id="login__pw" placeholder="">
+              <div class="login--form__find">
+                <a href="javascript:void(0)">아이디/패스워드 <span class="search">찾기</span></a>
+              </div>
             </div>
             <input type="submit" class="btn btn--primary" value="로그인">
             <span class="register__recommand">아직 회원이 아니신가요? <a href="javascript:void(0)">회원가입</a></span>
