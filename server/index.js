efda09767682956a4a0bf8a3d69301a5284d20da
jungu0416 <jungu0416@naver.com>
2025-01-17 23:48:20 +0900

    feat: 메뉴, 공지사항, 미팅, 근태 API 개발

--- server/index.js
+++ server/index.js
@@ -26,9 +26,8 @@ poolDb();
 
 // 로그인 체크
 app.post('/api/login', async (req, res) => {
-
   const { id } = req.body;
-  console.log(id);
+
   const query = 'SELECT * FROM USERS where id = ?';
   try {
     const conn = await poolDb();
@@ -40,58 +39,92 @@ app.post('/api/login', async (req, res) => {
   }
 })
 
+// 메뉴
+app.get('/api/menu/:role', async (req, res) => {
+  const { role } = req.params;
 
-// 메뉴 체크
+  const query = 'SELECT * FROM menu where role = ?';
+  try {
+    const conn = await poolDb();
+    const rows = await conn.query(query,[role]);
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('메뉴 조회 중 오류가 발생했습니다.');
+  }
+});
+
+// 공지사항
+app.get('/api/notice', async (req, res) => {
+  const query = 'SELECT * FROM notice';
+  try {
+    const conn = await poolDb();
+    const rows = await conn.query(query);
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('공지사항 조회 중 오류가 발생했습니다.');
+  }
+});
 
 
+// 미팅
+app.post('/api/meet', async (req, res) => {
+  const { num } = req.body;
+
+  const query = 'SELECT * FROM meeting where num = ? order by time asc';
+  try {
+    const conn = await poolDb();
+    const rows = await conn.query(query, [num]);
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('미팅 일정 조회 중 오류가 발생했습니다.');
+  }
+})
 
 
+// 근태
+app.post('/api/work', async (req, res) => {
+  const { num } = req.body;
 
-
-
-
-
-
-// 사용자 데이터 조회 GET 방식 예제
-// app.get('/users', async (req, res) => {
-//   const query = 'SELECT * FROM users';
-//   try {
-//     const conn = await getConnection();
-//     const rows = await conn.query(query);
-//     res.send(rows);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send('사용자 조회 중 오류가 발생했습니다.');
-//   }
-// });
-
+  const query = 'SELECT * FROM approve where num = ? order by start_date desc';
+  try {
+    const conn = await poolDb();
+    const rows = await conn.query(query, [num]);
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('미팅 일정 조회 중 오류가 발생했습니다.');
+  }
+})
 
 
 // json 파일에서 READ 예제
-// app.get('/api/users.json', (req, res) => {
-//   fs.readFile('./server/data/users.json', 'utf8', (err, data) => {
-//     if (err) {
-//         console.error('Error reading JSON file:', err)
-//         return res.status(500).send({ 
-//           status: 'Internal Server Error',
-//           message: err,
-//           data: null,
-//         })
-//     }
+app.get('/api/users.json', (req, res) => {
+  fs.readFile('./server/data/users.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading JSON file:', err)
+        return res.status(500).send({ 
+          status: 'Internal Server Error',
+          message: err,
+          data: null,
+        })
+    }
 
-//     try {
-//         const jsonData = JSON.parse(data)
-//         res.json(jsonData)
-//     } catch (parseErr) {
-//         console.error('Error parsing JSON file:', parseErr)
-//         return res.status(500).send({ 
-//           status: 'Internal Server Error',
-//           message: parseErr,
-//           data: null,
-//         })
-//     }
-//   })
-// })
+    try {
+        const jsonData = JSON.parse(data)
+        res.json(jsonData)
+    } catch (parseErr) {
+        console.error('Error parsing JSON file:', parseErr)
+        return res.status(500).send({ 
+          status: 'Internal Server Error',
+          message: parseErr,
+          data: null,
+        })
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`ready to ${port}`)
