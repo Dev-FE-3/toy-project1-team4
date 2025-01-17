96f18f89beca98fefdfc0a33263682c17dab249d
jungu0416 <jungu0416@naver.com>
2025-01-17 11:29:32 +0900

    feat: 로그인 css 네이밍, 띄어쓰기 수정

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,28 +4,26 @@ export const login = function (content) {
 
   content.innerHTML = `
     <section id="login">
-    <div class="logo">
-      <img src="./../public/images/img_logo.png" class="logo__img" alt="PPangGeut">
-    </div>
-    <div class="login--form">
-      <form action="javascipt:void(0)" method="post">
-          <p>로그인</p>
-          <div class="input-wrap">
-              <label for="input-01" class="input-label">아이디</label>
-              <input type="text" class="input-text" id="input-01" placeholder="사원번호 또는 이메일 주소를 입력해주세요">
-          </div>    
-          <div class="input-wrap">
-            <label for="input-01" class="input-label">패스워드</label>
-            <input type="password" class="input-text" id="input-01" placeholder="">
-            <a href="javascript:void(0)">아이디/패스워드 찾기</a>
-          </div>
-          <input type="submit" class="btn btn--primary" value="로그인">
-          <span>아직 회원이 아니신가요? <a href="javascript:void(0)">회원가입</a></span>
-      </form>
-    </div>
-
+      <div class="logo">
+        <img src="./../public/images/img_logo.png" class="logo__img" alt="PPangGeut">
+      </div>
+      <div class="login--form">
+        <form action="javascipt:void(0)" method="post">
+            <p class="login--form__title">로그인</p>
+            <div class="input-wrap">
+                <label for="input-01" class="input-label">아이디</label>
+                <input type="text" class="input-text" id="input-01" placeholder="사원번호 또는 이메일 주소를 입력해주세요">
+            </div>    
+            <div class="input-wrap">
+              <label for="input-01" class="input-label">패스워드</label>
+              <input type="password" class="input-text" id="input-01" placeholder="">
+              <a href="javascript:void(0)">아이디/패스워드 찾기</a>
+            </div>
+            <input type="submit" class="btn btn--primary" value="로그인">
+            <span class="register__recommand">아직 회원이 아니신가요? <a href="javascript:void(0)">회원가입</a></span>
+        </form>
+      </div>
     </section>
-
   `;
 
   const menu = document.querySelector('.menu');
