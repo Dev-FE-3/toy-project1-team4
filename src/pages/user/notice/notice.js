08c46f979d626d984bf15d8aba7d1e59f9b96e33
hgTime <hgtime@gmail.com>
2025-01-19 02:50:52 +0900

    feat: 공지사항 퍼블리싱

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -4,23 +4,98 @@ export const notice = function (content) {
 
   content.innerHTML = `
     <ul id="notice" class="row">
-      <li class= "card col-4 ">
-        <a href="#">
-          <div class="image-container">
-            <div src="./../public/images/img_IT.png" alt="게시글 이미지" class="card__img" /> 
+      <li class="notice-card col-4 ">
+        <a href="#" class="notice-card__link">
+          <div class="card-image__container">
+            <img src="./../public/images/img_IT.png" alt="게시글 이미지" class="card-image" /> 
           </div> 
-          <div class="">
-            <h4 class="">[신규 프로젝트 런칭 안내: Project Phoenix]</h4>
-            <p class="card__content">
+          <div class="card__details">
+            <h4 class="detail__title">[신규 프로젝트 런칭 안내: Project Phoenix ]</h4>
+            <p class="detail__description">
               본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
             </p>
           </div>
           <div class="card__author">
-            <img src="./../public/images/img_men.png" class="author-img" />
-            <div class="">
-              <div class="author-content">
+            <img src="./../public/images/img_men.png" class="author__img" />
+            <div class="author__details">
+              <div class="author__description">
                 <span class="author__position">백그라운드개발드림팀</span>
-                <span class="author__name">김수완</span>
+                <span class="author__name">김수완거북이와두루미삼척갑사동방석</span>
+              </div>
+              <time>
+                2025-01-14
+              </time>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="notice-card col-4 ">
+        <a href="#" class="notice-card__link">
+          <div class="card-image__container">
+            <img src="./../public/images/img_IT.png" alt="게시글 이미지" class="card-image" /> 
+          </div> 
+          <div class="card__details">
+            <h4 class="detail__title">[신규 프로젝트 런칭 안내: Project Phoenix ]</h4>
+            <p class="detail__description">
+              본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
+            </p>
+          </div>
+          <div class="card__author">
+            <img src="./../public/images/img_men.png" class="author__img" />
+            <div class="author__details">
+              <div class="author__description">
+                <span class="author__position">백그라운드개발드림팀</span>
+                <span class="author__name">김수완거북이와두루미삼척갑사동방석</span>
+              </div>
+              <time>
+                2025-01-14
+              </time>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="notice-card col-4 ">
+        <a href="#" class="notice-card__link">
+          <div class="card-image__container">
+            <img src="./../public/images/img_IT.png" alt="게시글 이미지" class="card-image" /> 
+          </div> 
+          <div class="card__details">
+            <h4 class="detail__title">[신규 프로젝트 런칭 안내: Project Phoenix ]</h4>
+            <p class="detail__description">
+              본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
+            </p>
+          </div>
+          <div class="card__author">
+            <img src="./../public/images/img_men.png" class="author__img" />
+            <div class="author__details">
+              <div class="author__description">
+                <span class="author__position">백그라운드개발드림팀</span>
+                <span class="author__name">김수완거북이와두루미삼척갑사동방석</span>
+              </div>
+              <time>
+                2025-01-14
+              </time>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="notice-card col-4 ">
+        <a href="#" class="notice-card__link">
+          <div class="card-image__container">
+            <img src="./../public/images/img_IT.png" alt="게시글 이미지" class="card-image" /> 
+          </div> 
+          <div class="card__details">
+            <h4 class="detail__title">[신규 프로젝트 런칭 안내: Project Phoenix ]</h4>
+            <p class="detail__description">
+              본사에서 신규 프로젝트인 "Project Phoenix"를 다음 달부터 진행합니다. 참여 희망자는 개발팀 김민재 팀장에게 문의주세요
+            </p>
+          </div>
+          <div class="card__author">
+            <img src="./../public/images/img_men.png" class="author__img" />
+            <div class="author__details">
+              <div class="author__description">
+                <span class="author__position">백그라운드개발드림팀</span>
+                <span class="author__name">김수완거북이와두루미삼척갑사동방석</span>
               </div>
               <time>
                 2025-01-14
@@ -32,3 +107,24 @@ export const notice = function (content) {
     </ul>
   `;
 }
+
+{/* 페이지네이션
+    <section class="pagination">
+      <a href="#" class="pagination--prev">
+        prev
+      </a>
+      <a href="#" class="active">
+        1
+      </a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">7</a>
+      <a href="#">8</a>
+      <a href="#" class="pagination--next">
+        next
+      </a>
+    </section>
+    */}
