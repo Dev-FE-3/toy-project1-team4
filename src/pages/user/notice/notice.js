169d5d0dd30570e17c5efc77d9b8d354a77f6e23
hgTime <hgtime@gmail.com>
2025-01-20 00:53:13 +0900

    feat: 공지사항 json데이터 수 만큼 카드 렌더링 기능 구현

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -2,46 +2,44 @@ import './notice.css';
 import noticeData from '../../../../server/data/notice.json';
 
 export const notice = function (content) {
-  const noticeItems = noticeData.data[0];
-  const cardId = noticeItems.cardId;
-  const cardImg = noticeItems.cardImg;
-  const cardTitle = noticeItems.cardTitle;
-  const cardDescription = noticeItems.cardDescription;
-  const authorImg = noticeItems.authorImg;
-  const authorPosition = noticeItems.authorPosition;
-  const authorName = noticeItems.authorName;
-  const date = noticeItems.date;
+  const noticeItems = noticeData.data;
+  
+  const cardsHTML = noticeItems.map((item) => {
+    const { cardId, cardImg, cardTitle, cardDescription, authorImg, authorPosition, authorName, date } = item;
+  
+    return `
+        <li class="notice-card col-4" id="${cardId}">
+          <a href="#" class="notice-card__link">
+            <div class="card-image__container">
+              <img src="${cardImg}" alt="게시글 이미지" class="card-image" />
+            </div>
+            <div class="card__details">
+              <h4 class="detail__title">${cardTitle}</h4>
+              <p class="detail__description">${cardDescription}</p>
+            </div>
+            <div class="card__author">
+              <img src="${authorImg}" class="author__img" />
+              <div class="author__details">
+                <div class="author__description">
+                  <span class="author__position">${authorPosition}</span>
+                  <span class="author__name">${authorName}</span>
+                </div>
+                <time>${date}</time>
+              </div>
+            </div>
+          </a>
+        </li>
+      `;
+    })
+    .join(''); 
 
   content.innerHTML = `
     <ul id="notice" class="row">
-      <li class="notice-card col-4" id="${cardId}">
-        <a href="#" class="notice-card__link">
-          <div class="card-image__container">
-            <img src="${cardImg}" alt="게시글 이미지" class="card-image" /> 
-          </div> 
-          <div class="card__details">
-            <h4 class="detail__title">${cardTitle}</h4>
-            <p class="detail__description">
-            ${cardDescription}
-            </p>
-          </div>
-          <div class="card__author">
-            <img src="${authorImg}" class="author__img" />
-            <div class="author__details">
-              <div class="author__description">
-                <span class="author__position">${authorPosition}</span>
-                <span class="author__name">${authorName}</span>
-              </div>
-              <time>
-              ${date}
-              </time>
-            </div>
-          </div>
-        </a>
-      </li>
+      ${cardsHTML}
     </ul>
   `;
-}
+};
+
 
 {/* 페이지네이션
     <section class="pagination">
