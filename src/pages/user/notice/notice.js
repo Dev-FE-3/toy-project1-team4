0330019e6c6b91b36493491bd052eac35465cd24
hgTime <hgtime@gmail.com>
2025-01-20 14:55:32 +0900

    feat: 공지사항 퍼브리싱 수정

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -5,26 +5,25 @@ export const notice = function (content) {
   const noticeItems = noticeData.data;
   
   const cardsHTML = noticeItems.map((item) => {
-    const { cardId, cardImg, cardTitle, cardDescription, authorImg, authorPosition, authorName, date } = item;
   
     return `
-        <li class="notice-card col-4" id="${cardId}">
+        <li class="notice-card col-4" data--notion--num="${item.cardNum}">
           <a href="#" class="notice-card__link">
             <div class="card-image__container">
-              <img src="${cardImg}" alt="게시글 이미지" class="card-image" />
+              <img src="${item.cardImg}" alt="게시글 이미지" class="card-image" />
             </div>
             <div class="card__details">
-              <h4 class="detail__title">${cardTitle}</h4>
-              <p class="detail__description">${cardDescription}</p>
+              <h4 class="detail__title">${item.cardTitle}</h4>
+              <p class="detail__description">${item.cardDescription}</p>
             </div>
             <div class="card__author">
-              <img src="${authorImg}" class="author__img" />
+              <img src="${item.authorImg}" class="author__img" />
               <div class="author__details">
                 <div class="author__description">
-                  <span class="author__position">${authorPosition}</span>
-                  <span class="author__name">${authorName}</span>
+                  <span class="author__position">${item.authorPosition}</span>
+                  <span class="author__name">${item.authorName}</span>
                 </div>
-                <time>${date}</time>
+                <time>${item.date}</time>
               </div>
             </div>
           </a>
@@ -61,25 +60,3 @@ export const notice = function (content) {
     </div>
   `;
 };
-
-
-{/* 페이지네이션
-    <section class="pagination">
-      <a href="#" class="pagination--prev">
-        prev
-      </a>
-      <a href="#" class="active">
-        1
-      </a>
-      <a href="#">2</a>
-      <a href="#">3</a>
-      <a href="#">4</a>
-      <a href="#">5</a>
-      <a href="#">6</a>
-      <a href="#">7</a>
-      <a href="#">8</a>
-      <a href="#" class="pagination--next">
-        next
-      </a>
-    </section>
-    */}
