85bb4e07ffc86c694e9033db2c5248e5391a9f7d
minsung <mingsung521@gmail.com>
2025-01-20 18:44:01 +0900

    feat: pagination move 버튼 기능 추가 구현

--- src/pages/admin/employee/list/employee-list.js
+++ src/pages/admin/employee/list/employee-list.js
@@ -20,15 +20,23 @@ const employees = rawEmployeeData.data.map(function (rawEmployee) {
           `;
 });
 
-const listLength = 9;
-const initialIndex = 1;
-const totalIndex = Math.ceil(employees.length / listLength);
+const listLength = 9; // 한 페이지에 들어갈 직원 수
+const indexLength = 8; // pagination bar 에 표시할 최대 인덱스 버튼 개수
+const initialIndex = 1; // 처음 접속 시 인덱스 값
+const totalIndex = Math.ceil(employees.length / listLength); // 총 인덱스 개수(리스트 요소 개수를 기준)
+let paginationBarIndex = 1; // 인덱스 버튼세트 리스트 값 1~8 : 1, 9~16 : 2 ...
 let currentIndex = initialIndex;
+
+// console.log(totalIndex);
 
 export const employeeList = function (content) {
   const paginationBtnsRender = function () {
     const paginationBtnList = [];
-    for (let i = 1; i <= totalIndex; i++) {
+    for (
+      let i = 1 + indexLength * (paginationBarIndex - 1);
+      i <= indexLength * paginationBarIndex && i <= totalIndex;
+      i++
+    ) {
       paginationBtnList[i] = `<a href="javascript:;"
       data-btn-index = "${i}"
       class="pagination--index ${i == currentIndex ? 'active' : ''}">${i}</a>`;
@@ -37,14 +45,45 @@ export const employeeList = function (content) {
   };
 
   const paginationBtnsAddEvent = function () {
-    const btnObjects = document.querySelectorAll('.pagination--index');
-    btnObjects.forEach(function (btnObject) {
-      btnObject.addEventListener('click', function () {
-        // console.log(btnObject.dataset.btnIndex);
-        currentIndex = btnObject.dataset.btnIndex;
+    const indexBtn = document.querySelectorAll('.pagination--index');
+    indexBtn.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        // console.log(btn.dataset.btnIndex);
+        currentIndex = btn.dataset.btnIndex;
         triggerRender(content); // 상태값이 변경되었으므로 페이지 렌더 함수를 재호출
       });
     });
+
+    const moveBtn = {
+      prev: document.querySelector('.pagination--prev'),
+      next: document.querySelector('.pagination--next'),
+    };
+
+    moveBtn.prev.addEventListener('click', function () {
+      console.log('prev clicked');
+      if (currentIndex > indexLength) {
+        currentIndex -= indexLength;
+        paginationBarIndex--;
+      } else {
+        currentIndex = 1;
+      }
+
+      triggerRender(content);
+    });
+    moveBtn.next.addEventListener('click', function () {
+      if (totalIndex > indexLength) {
+        if (currentIndex > Math.floor(totalIndex / indexLength) * indexLength) {
+          currentIndex = totalIndex;
+        } else {
+          currentIndex = indexLength * paginationBarIndex + 1;
+          paginationBarIndex++;
+        }
+      } else {
+        currentIndex = totalIndex;
+      }
+      triggerRender(content);
+    });
+
     return;
   };
 
