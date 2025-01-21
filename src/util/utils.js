06cca36e8019cfc682f0e714bab6c8456f735a1b
jungu0416 <jungu0416@naver.com>
2025-01-21 10:01:52 +0900

    feat: getType 추가

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,37 +1,41 @@
 /*
-* 로그인 세션 or 토큰 상태 확인
-* 유효성 검사
-* 정규표현식
-* 숫자 콤마 
-* 전화번호 포맷
-* 랜덤 생성 함수
-* 등등
-*/
+ * 로그인 세션 or 토큰 상태 확인
+ * 유효성 검사
+ * 정규표현식
+ * 숫자 콤마
+ * 전화번호 포맷
+ * 랜덤 생성 함수
+ * 등등
+ */
 
-export { formatDateTime, approveStatusStyle }
+export { formatDateTime, approveStatusStyle, getType };
 
-//날짜 포멧 함수
+// 데이터 타입 확인
+function getType(data) {
+  return Object.prototype.toString.call(data).slice(8, -1);
+}
+
+// 날짜 포멧 함수
 function formatDateTime(isoString) {
   // ISO 날짜 문자열을 JavaScript Date 객체로 변환
   const date = new Date(isoString);
-  
+
   // 연도, 월, 일 추출
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  
+
   // 시각 추출
   // const hours = String(date.getHours()).padStart(2, '0');
   // const minutes = String(date.getMinutes()).padStart(2, '0');
-  
+
   // 원하는 형식으로 반환
   // return `${year}-${month}-${day} ${hours}:${minutes}`;
   return `${year}-${month}-${day}`;
 }
 
-
-//결제 버튼 css style class
-function approveStatusStyle (str) {
+// 결제 버튼 css style class
+function approveStatusStyle(str) {
   switch (str) {
     case '결제 중':
       str = 'label--purple';
