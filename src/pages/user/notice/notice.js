bbddf4a2d8ce4063730d678677a4c1cf7b2863da
hgTime <hgtime@gmail.com>
2025-01-23 11:00:56 +0900

    feat: 페이징 기능 구현

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -51,27 +51,89 @@ export const notice = function (content) {
           </ul>
         </div>
         <section class="pagination">
-            <a href="javascript:;" class="pagination--prev">prev</a> 
-            <a href="javascript:;" class="pagination--index active">1</a>
-            <a href="javascript:;" class="pagination--index">2</a>
-            <a href="javascript:;" class="pagination--index">3</a>
-            <a href="javascript:;" class="pagination--next">next</a>
+            
           </section>
       </div>
     </div>
   `;
 
-  async function getNoticeData() {
-    let noticeCard = document.querySelector('.notice-card');
+  // async function getNoticeData() {
+  //   let noticeCard = document.querySelector('.notice-card');
 
-    // 비동기로 data 가져오기
+  //   // 비동기로 data 가져오기
+  //   try {
+  //     const response = await axios.get('api/notice');
+
+  //     noticeCard.innerHTML = response.data
+  //       .map(item => {
+  //         return `
+  //           <li class="notice-card col-4" data-notice-num="${item.noticeNum}">
+  //             <a href="#" class="notice-card__link">
+  //               <div class="card-image__container">
+  //                 <div class="card-image__style" style="background-image: url(${
+  //                   item.img_path
+  //                 })"></div>
+  //               </div>
+  //               <div class="card__details">
+  //                 <h4 class="detail__title">${item.title}</h4>
+  //                 <p class="detail__description">${item.content}</p>
+  //               </div>
+  //               <div class="card__author">
+  //                 <img src="${item.authorImg}" class="author__img" />
+  //                 <div class="author__details">
+  //                   <div class="author__description">
+  //                     <span class="author__position">${item.department}</span>
+  //                     <span class="author__name">${item.name}</span>
+  //                   </div>
+  //                   <time>${formatDateTime(item.insert_date)}</time>
+  //                 </div>
+  //               </div>
+  //             </a>
+  //           </li>
+  //         `;
+  //       })
+  //       .join('');
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  // getNoticeData();
+
+  // 페이징처리
+  const itemsPerPage = 6;
+  let allCardItem = [];
+  let nowPage = 1;
+
+  // 비동기로 data 가져오기
+  async function getNoticeData() {
     try {
       const response = await axios.get('api/notice');
+      allCardItem = response.data;
+      console.log('allCardItem확인:   ' + JSON.stringify(allCardItem));
+      renderNoticeCard(nowPage);
+      cardPagingBtn();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-      noticeCard.innerHTML = response.data
-        .map(item => {
-          return `
-            <li class="notice-card col-4" data-notice-num="${item.noticeNum}">
+  // 각 페이지에 해당하는 카드 데이터 만들기
+  function renderNoticeCard(page) {
+    let noticeCard = document.querySelector('.notice-card');
+
+    // 카드 6개씩 만듬
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = page * itemsPerPage;
+    const pageCardData = allCardItem.slice(startIndex, endIndex);
+    console.log('startIndex##########:', startIndex);
+    console.log('endIndex 수??!?!?!??:', endIndex);
+
+    console.log('6개 cardItem확인:   ' + JSON.stringify(pageCardData));
+
+    noticeCard.innerHTML = pageCardData
+      .map(item => {
+        return `
+            <li class="notice-card col-4" data-notice-num="${item.notice_num}">
               <a href="#" class="notice-card__link"> 
                 <div class="card-image__container">
                   <div class="card-image__style" style="background-image: url(${
@@ -95,11 +157,52 @@ export const notice = function (content) {
               </a>
             </li>
           `;
-        })
-        .join('');
-    } catch (error) {
-      console.error(error);
+      })
+      .join('');
+  }
+
+  // 페이지네이션 버튼 만들기
+  function cardPagingBtn() {
+    let pagination = document.querySelector('.pagination');
+    let paginationBar = '';
+    const totalPages = Math.ceil(allCardItem.length / itemsPerPage);
+
+    console.log('총 데이터 수:', allCardItem.length);
+    console.log('itemsPerPage:', itemsPerPage);
+    console.log('계산된 totalPages:', totalPages);
+
+    // pre 버튼
+    if (nowPage > 1) {
+      paginationBar += `<a href="#" class="pagination--prev" data-page="${
+        nowPage - 1
+      }">prev</a> `;
     }
+
+    // 페이지 index 번호 버튼
+    for (let i = 1; i <= totalPages; i++) {
+      paginationBar += `<a href="#" class="pagination--index ${
+        i === nowPage ? 'active' : ''
+      }" data-page="${i}">${i}</a>`;
+    }
+
+    //next 버튼
+    if (nowPage < totalPages) {
+      paginationBar += `<a href="#" class="pagination--next" data-page="${
+        nowPage + 1
+      }">next</a>`;
+    }
+
+    pagination.innerHTML = paginationBar;
+
+    // 버튼 클릭 이벤트
+    document.querySelectorAll('.pagination a').forEach(button => {
+      button.addEventListener('click', () => {
+        console.log('클릭됨');
+        const clickBtn = parseInt(button.getAttribute('data-page'));
+        nowPage = clickBtn;
+        renderNoticeCard(nowPage);
+      });
+    });
   }
   getNoticeData();
 };
