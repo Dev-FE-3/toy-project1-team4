303ac757b5b93c8fd9a9f1a65833e837168acf37
hgTime <hgtime@gmail.com>
2025-01-23 11:04:31 +0900

    fix: 페이징에 관한 불필요한 주석 제거

--- src/pages/user/notice/notice.js
+++ src/pages/user/notice/notice.js
@@ -57,48 +57,6 @@ export const notice = function (content) {
     </div>
   `;
 
-  // async function getNoticeData() {
-  //   let noticeCard = document.querySelector('.notice-card');
-
-  //   // 비동기로 data 가져오기
-  //   try {
-  //     const response = await axios.get('api/notice');
-
-  //     noticeCard.innerHTML = response.data
-  //       .map(item => {
-  //         return `
-  //           <li class="notice-card col-4" data-notice-num="${item.noticeNum}">
-  //             <a href="#" class="notice-card__link">
-  //               <div class="card-image__container">
-  //                 <div class="card-image__style" style="background-image: url(${
-  //                   item.img_path
-  //                 })"></div>
-  //               </div>
-  //               <div class="card__details">
-  //                 <h4 class="detail__title">${item.title}</h4>
-  //                 <p class="detail__description">${item.content}</p>
-  //               </div>
-  //               <div class="card__author">
-  //                 <img src="${item.authorImg}" class="author__img" />
-  //                 <div class="author__details">
-  //                   <div class="author__description">
-  //                     <span class="author__position">${item.department}</span>
-  //                     <span class="author__name">${item.name}</span>
-  //                   </div>
-  //                   <time>${formatDateTime(item.insert_date)}</time>
-  //                 </div>
-  //               </div>
-  //             </a>
-  //           </li>
-  //         `;
-  //       })
-  //       .join('');
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-  // getNoticeData();
-
   // 페이징처리
   const itemsPerPage = 6;
   let allCardItem = [];
